19103d78e70dc675420072d21dd85620fabe447b
ken-cronos <104898245+ken-cronos@users.noreply.github.com>
2024-09-03 23:10:17 -0400

    add oracle sources

--- app/api/rates/route.ts
+++ app/api/rates/route.ts
@@ -20,13 +20,16 @@ export async function GET() {
     const ethUsd = coingeckoApiResult["ethereum"].usd;
     const daiUsd = coingeckoApiResult["dai"].usd;
     const lcroUsd = coingeckoApiResult["liquid-cro"].usd;
-    // Hardcoded values for testing:
+
+    // These are just some hardcoded values for testing
     // const croUsd = 0.08;
     // const ethUsd = 2500;
     // const daiUsd = 1;
     // const lcroUsd = 0.09;
+
     // Call smart contracts on Ethereum
     const w3provider = new ethers.JsonRpcProvider(process.env.ETHEREUM_URL);
+
     // Get zkCRO : CRO rate. Note that CRO has 8 decimals on Ethereum
     let contractAddress = "0x28Ff2E4dD1B58efEB0fC138602A28D5aE81e44e2";
     let abi = [
@@ -53,6 +56,7 @@ export async function GET() {
     let chainCallResString = ethers.formatUnits(chainCallRes, resultDecimals);
     let zkCroCro = parseFloat(chainCallResString);
     let zkCroUsd = zkCroCro * croUsd;
+
     // Get ybETH
     contractAddress = "0x76bf2D1e6dFda645c0c17440B17Eccc181dfC351";
     abi = [
@@ -78,6 +82,7 @@ export async function GET() {
     chainCallResString = ethers.formatUnits(chainCallRes, resultDecimals);
     let ybEthEth = parseFloat(chainCallResString);
     let ybEthUsd = ybEthEth * ethUsd;
+
     // Get ybUSD
     contractAddress = "0xFA59075DfCE274E028b58BdDFcC3D709960F594a";
     abi = [
@@ -104,11 +109,20 @@ export async function GET() {
     chainCallResString = ethers.formatUnits(chainCallRes, resultDecimals);
     let ybUsdDai = parseFloat(chainCallResString);
     let ybUsdUsd = ybUsdDai * daiUsd;
-    // Include pegged values
-    const vEthEth = 1;
-    const vUsdDai = 1;
-    const vEthUsd = vEthEth * ethUsd;
-    const vUsdUsd = vUsdDai * daiUsd;
+
+    // Get vETH and vUSD from experimental Oracle
+    let oracleResponse = await fetch(
+        "https://oracle-zkevm.cronos.org/api/v1/query/vUSD-USD"
+    );
+    let oracleResult = await oracleResponse.json();
+    const vUsdUsd = oracleResult.result.aggregatedPrice;
+    const vUsdDai = vUsdUsd / daiUsd;
+    oracleResponse = await fetch(
+        "https://oracle-zkevm.cronos.org/api/v1/query/vETH-USD"
+    );
+    oracleResult = await oracleResponse.json();
+    const vEthUsd = oracleResult.result.aggregatedPrice;
+    const vEthEth = vEthUsd / ethUsd;
     // Send back response
     return NextResponse.json({
         success: true,
